63fbd69789b39fbbde0ccac83331bed720d124e7
AlexSniper <udau@bk.ru>
2020-04-11 13:23:12 -0400

    This application will save your notes

--- app/src/main/java/com/example/zapisnajaknizka/MainActivity.java
+++ app/src/main/java/com/example/zapisnajaknizka/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.zapisnajaknizka;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -14,26 +18,69 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class MainActivity extends AppCompatActivity {
   static   ArrayList<String> notes = new ArrayList<>();
   static   ArrayAdapter arrayAdapter;
+
+    SharedPreferences sharedPreferences;//Here
+    //  SharedPreferences is created
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        sharedPreferences = getApplicationContext().getSharedPreferences("com.example.zapisnajaknizka", Context.MODE_PRIVATE);// Here we are getting
+        // access to shared preferences
         ListView listView  = findViewById(R.id.listView);
-        notes.add("Example note");
+        HashSet<String>set = (HashSet<String>) sharedPreferences.getStringSet("notes", null);
+
+
+        if (set == null){
+            notes.add("Dobavte zametku sdes");
+        }
+        else notes = new ArrayList(set);// it will create new arrayList from information in that set
+        notes.add("Dobavte zametku sdes");
         arrayAdapter = new ArrayAdapter( this, android.R.layout.simple_list_item_1, notes);// simple_list_item_1
         // gives simple Layout  , notes is specifies with which array we are working with
         listView.setAdapter(arrayAdapter);
-listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
         Intent intent = new Intent (getApplicationContext(), NoteEditorActivity.class);
         intent .putExtra("noteId", i);
         startActivity(intent);
+    }
+});
+
+listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+
+        final int itemToDelete = position;
+
+       new  AlertDialog. Builder(MainActivity.this)
+               .setIcon(android.R.drawable.ic_dialog_alert)
+       .setTitle("Are you sure?")
+               .setMessage("Do you want to delete this note")
+               .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                   @Override
+                   public void onClick(DialogInterface dialog, int i) {
+
+                       notes.remove(itemToDelete);// removing as item from arrayList
+                     arrayAdapter.notifyDataSetChanged();//Notifying arrayAdapter that item was deleted
+
+                       HashSet<String> set = new HashSet<>(MainActivity.notes);// Here we converting arrayList into a HashSet , and then HashSet is saved in shared preferences
+                       sharedPreferences.edit().putStringSet("notes", set).apply();
+
+                   }
+               })
+               .setNegativeButton("No", null)
+              .show();
+        return true;
     }
 });
     }
--- app/src/main/java/com/example/zapisnajaknizka/NoteEditorActivity.java
+++ app/src/main/java/com/example/zapisnajaknizka/NoteEditorActivity.java
@@ -2,23 +2,31 @@ package com.example.zapisnajaknizka;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.EditText;
 
-public class NoteEditorActivity extends AppCompatActivity {
+import java.util.HashSet;
 
+public class NoteEditorActivity extends AppCompatActivity {
+     int noteId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_editor);
         EditText editText = findViewById(R.id.editText);
         Intent intent = getIntent();
-        final int noteId = intent.getIntExtra("noteId", -1);// -1 because nomber which could be passed in could be as low as zero
+     noteId = intent.getIntExtra("noteId", -1);// -1 because number which could be passed in could be as low as zero
         if (noteId != -1 ){
             editText.setText(MainActivity.notes.get(noteId));
+        }
+        else {
+           MainActivity.notes.add("");
+           noteId = MainActivity.notes.size() -1;
         }
         editText.addTextChangedListener(new TextWatcher() {
             @Override
@@ -30,6 +38,10 @@ public class NoteEditorActivity extends AppCompatActivity {
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 MainActivity.notes.set(noteId, String.valueOf(s));// If someone did updates this code will perform updates inside of array
                 MainActivity.arrayAdapter.notifyDataSetChanged();
+
+                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.zapisnajaknizka", Context.MODE_PRIVATE);
+                HashSet<String> set = new HashSet<>(MainActivity.notes);// Here we converting arrayList into a hash set , and they HashSet is saved in shared preferences
+           sharedPreferences.edit().putStringSet("notes", set).apply();
             }
 
             @Override
